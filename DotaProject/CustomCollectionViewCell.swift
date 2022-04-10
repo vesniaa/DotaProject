72c3984f4b57e9481f398ff61a51e90a4aba91b5
Eugenia <pyssiki@yandex.ru>
2022-04-10 23:30:02 +0500

    customer

---
+++ DotaProject/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  DotaProject
+//
+//  Created by Евгения Аникина on 10.04.2022.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
