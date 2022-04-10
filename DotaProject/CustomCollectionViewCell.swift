f73fa8f1da94c36bab992f4140c4013760e58a3b
Eugenia <pyssiki@yandex.ru>
2022-04-10 23:39:08 +0500

    new code

--- DotaProject/CustomCollectionViewCell.swift
+++ DotaProject/CustomCollectionViewCell.swift
@@ -8,5 +8,6 @@
 import UIKit
 
 class CustomCollectionViewCell: UICollectionViewCell {
-    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var namelabel: UILabel!
 }
---
+++ DotaProject/Hero.swift
@@ -0,0 +1,13 @@
+//
+//  Hero.swift
+//  DotaProject
+//
+//  Created by Евгения Аникина on 10.04.2022.
+//
+
+import Foundation
+
+struct Hero: Decodable {
+    let localized_name: String
+    let img: String
+}
